75c56213130b1ed012efd39c8fc77d25de8e51f0
Kleyton Santos <85530567+KleytonFSantos@users.noreply.github.com>
2022-05-31 03:41:52 -0300

    Update api
    up to date

--- routes/api.php
+++ routes/api.php
@@ -19,8 +19,8 @@ Route::group([
     'middleware' => 'api',
     'prefix' => 'auth'
 ], function ($router) {
-    Route::post('/login', [AuthController::class, 'login']);
     Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::post('/refresh', [AuthController::class, 'refresh']);
 });
